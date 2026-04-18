3de3c5f70dbdde27695672e1fcc8c4a50121bd72
aphlar <aphlar17@icloud.com>
2026-04-18 13:19:09 -0600

    Change: first commit with angular

---
+++ src/app/pages/elements-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elements-page',
+  standalone: true,
+  templateUrl: './elements-page.component.html'
+})
+export class ElementsPageComponent {}
---
+++ src/app/pages/generic-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-page',
+  standalone: true,
+  templateUrl: './generic-page.component.html'
+})
+export class GenericPageComponent {}
