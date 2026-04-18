f8a7f207a77a38abece3ed80e7e74c6b98ba5984
aphlar <aphlar17@icloud.com>
2026-04-18 13:47:45 -0600

    Change: deploy qa branch

--- src/app/pages/elements-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-elements-page',
-  standalone: true,
-  templateUrl: './elements-page.component.html'
-})
-export class ElementsPageComponent {}
--- src/app/pages/generic-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-generic-page',
-  standalone: true,
-  templateUrl: './generic-page.component.html'
-})
-export class GenericPageComponent {}
